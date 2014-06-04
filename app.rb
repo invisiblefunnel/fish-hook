9e57e9ddb1400ee788747a784788dc9373183170
Danny Whalen <daniel.r.whalen@gmail.com>
2014-06-04 15:57:05 -0700

    Improved

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require 'dotenv'
 Dotenv.load
 
-require './db'
 require 'json'
+require 'sequel'
 require 'sinatra'
+
+DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
+
+DB.create_table?(:hooks) do
+  primary_key :id
+  Integer :subscriber_id, index: true
+  String :payload
+  DateTime :created_at
+end
 
 if ENV['BASIC_AUTH_NAME'] && ENV['BASIC_AUTH_PASSWORD']
   use Rack::Auth::Basic, 'Restricted Area' do |name, password|
@@ -11,7 +20,12 @@ if ENV['BASIC_AUTH_NAME'] && ENV['BASIC_AUTH_PASSWORD']
   end
 end
 
+get '/' do
+  @hooks = DB[:hooks].limit(500)
+  erb :index
+end
+
 post '/hooks/:subscriber_id' do
-  DB[:hooks].insert(subscriber_id: params[:subscriber_id], payload: request.body.read)
+  DB[:hooks].insert(subscriber_id: params[:subscriber_id], payload: request.body.read, created_at: DateTime.now)
   200
 end
--- db.rb
+++
@@ -1,9 +0,0 @@
-require 'sequel'
-
-DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
-
-DB.create_table?(:hooks) do
-  primary_key :id
-  Integer :subscriber_id, index: true
-  String :payload
-end
